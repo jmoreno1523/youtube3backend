b0b80c383afa775921e34ca1a660f993900ea0f2
jmoreno1523 <julian.moreno01@unicatolica.edu.co>
2024-11-26 12:09:11 -0500

    dasd

--- db/mongo.js
+++ db/mongo.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const uri = process.env.MONGO_URI
 //const uri = 'mongodb+srv://pocketuxdev:IwzOP4OGwzfoL8vz@cluster0.8wfjjdf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Crasdeate a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
